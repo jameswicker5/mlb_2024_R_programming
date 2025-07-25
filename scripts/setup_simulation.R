bddc9e1df74835ecb4bc310e30342cee6210b3bc
James Wicker <jameswicker555@gmail.com>
2025-07-24 17:23:24 -0700

    Update setup_simulation.R

--- scripts/setup_simulation.R
+++ scripts/setup_simulation.R
@@ -5,7 +5,7 @@ library(gt)
 library(purrr)
 
 # Load Data
-bat_data <- read_csv("C:/Users/james/OneDrive/Desktop/Portfolio/R/data/mlb_bat_2024.csv") %>%
+bat_data <- read_csv("/data/mlb_bat_2024.csv") %>%
 	filter(!team %in% c("2TM", "3TM"))
 
 # Create player-level probabilities
